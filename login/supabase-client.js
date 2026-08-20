269446ea0ebd5834e8d258baf044c9a756c009b1
Yann Kerherve <ya.kerherve@gmail.com>
2026-08-20 10:35:17 +0200

    Refactor Supabase client initialization

--- login/supabase-client.js
+++ login/supabase-client.js
@@ -3,4 +3,5 @@ const SUPABASE_URL = "https://merugolympcngamjukzo.supabase.co" ;
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lcnVnb2x5bXBjbmdhbWp1a3pvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMDM2MTQsImV4cCI6MjEwMjc3OTYxNH0.N1qeRzRv6vcUFEk4CucMMQarnnIG0P_6OgAwy451saM" ;
 
 // Initialisation globale
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY) ;
+const { createClient } = window.supabase ;
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY) ;
